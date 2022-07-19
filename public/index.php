127c1bc046533d1dd989430290f40275bad6cced
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-19 15:11:17 +0200

    modification on index file

--- public/index.php
+++ public/index.php
@@ -8,12 +8,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/DonkeyCar/public/assets/DONcarkey.png" type="image/x-icon">
     <title>Donkey Car</title>
 </head>
 <body>
+    <div>
+        <button  type="submit" name="submit"><a href="">Location</a></button>
+        <button  type="submit" name="submit"><a href="">Véhicule</a></button>
+        <button  type="submit" name="submit"><a href="">Contact</a></button>
+        <input id="searchbar" type="text" name="search" placeholder="recherche">
+    </div>
+
     <h1>Welcome to Donkey Car Rent Online</h1>
     <div>
-        <form>
+        <form action="" method="POST">
             <label>vehicule</label>       
             <input id="searchbar" type="text" name="search" placeholder="recherche">
             <label>du</label><input id="departure" type="date">
