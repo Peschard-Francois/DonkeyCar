61cbaa111558c101ba5879822a3c42ca4022f2ee
N0mekop <blinkevin@hotmail.com>
2022-07-19 12:45:36 +0200

    Compte + avatar

--- include/header.php
+++ include/header.php
@@ -23,19 +23,33 @@
                         <a class="nav-link active" aria-current="page" href="#">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Location</a>
+                        <a class="nav-link active" href="#">Location</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Véhicules</a>
+                        <a class="nav-link active" href="#">Véhicules</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../form.php">Contact</a>
+                        <a class="nav-link active" href="../form.php">Contact</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Rechercher</button>
                 </form>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Compte</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item nav-link active" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
+                    </li>
+                </ul>
+
             </div>
         </div>
     </nav>
