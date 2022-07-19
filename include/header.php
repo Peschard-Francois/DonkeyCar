5ca3eac62d136ffe33d2989451a7e87ba65c43fd
N0mekop <blinkevin@hotmail.com>
2022-07-19 14:59:03 +0200

    useraccount work in progress + some header updates

--- include/header.php
+++ include/header.php
@@ -38,7 +38,7 @@
                 </form>
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Compte</a>
+                        <a class="nav-link active" href="/useraccount.php">Compte</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav mb-2 mb-lg-0">
