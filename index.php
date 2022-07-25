6616805756a2fab2878e8773fad6c94d5e44bb64
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-25 11:26:27 +0200

    update CSS + add some assets

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ foreach ($dateReservation as $childArray)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/src/css/style.css">
     <link rel="icon" href=DONcarkey.png" type="image/x-icon">
     <title>Donkey Car</title>
 </head>
@@ -47,19 +48,17 @@ foreach ($dateReservation as $childArray)
 </header>
 <body>
 
-    <h1>Welcome to Donkey Car Rent Online</h1>
-    <div>
+<h1><span>Donkey</span><span>Car</span></h1>
+    <h4><span>roulez </span><span>comme </span><span>vous </span><span>êtes</span></h4>
+    <div class= "researchZone">
         <form action="" method="POST">
-            <label>vehicule</label>
             <select name="type" id="typ">
-                <option value="">Please choose an option</option>
+                <option value="">Veuillez choisir un type de vehicule</option>  
                 <?php foreach ($type as $types) : ?>
                     <option value="<?= $types['idtype'] ?>"><?= $types['nameType'] ?></option>
                 <?php endforeach ?>
             </select>
-            <label for="departure">du</label>
-            <input name="depart" id="departure" type="date">
-            <label for="arrival">au</label>
+            <input name="depart" id="departure" type="date" placeholder="du">
             <input name="arrive" id="arrival" type="date">
             <button type="submit">Rechercher</button>
 
@@ -69,24 +68,13 @@ foreach ($dateReservation as $childArray)
 
 
     <div  class="listeCars">
-
-       <!-- --><?php /*var_dump($singleArray[0]);
-
-        */?>
-            <?php foreach ($searchCar as $searchCars) : ?>
-                     <?php foreach ($dateReservation as $dateReservations) : ?>
-                            <?php if ($departPost < $dateReservations['dateReservationDebut']  AND $departPost > $dateReservations['dateReservationFin'] AND $dateReservations['vehicle_idvehicle'] === $searchCars['idvehicle'] ) :?>
-                             <h3>Vehicule Libre</h3><?= $searchCars['idvehicle']?> <?= $searchCars['brandVehicle']?>
-
-
-                    <?php endif ?>
-                <?php endforeach ?>
-            <?php endforeach ?>
-
-
-
-
-    <?= $arrivePost?>
+        <?php foreach ($searchCar as $searchCars) : ?>
+            <div class="carContainer">
+                <img class="imgCars" src="<?= $searchCars['imgVehicle']?>" alt="Image du véhicule">
+                <div class="carInfo"><?= $searchCars['brandVehicle']?>  <?= $searchCars['modelsVehicle']?></div>
+            </div>
+        <?php endforeach ?>
     </div>
+
 </body>
 </html>
