37e10cd189f5270973ee26e75eeb422c6fb5357c
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-26 09:03:32 +0200

    creation home.CSS and update link on home page

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/css/style.css">
+    <link rel="stylesheet" href="/src/css/home.css">
     <link rel="icon" href=DONcarkey.png" type="image/x-icon">
     <title>Donkey Car</title>
 </head>
