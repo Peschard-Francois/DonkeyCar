bfcefb520edcf1875d6d14747441c5fb733d499e
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-26 15:49:56 +0200

    change link home.css to index.css

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/css/home.css">
+    <link rel="stylesheet" href="/src/css/index.css">
     <link rel="icon" href="/src/css/assets/donkeycar.png" type="image/x-icon">
     <title>Donkey Car</title>
 </head>
