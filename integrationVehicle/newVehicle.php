5fc93665b73c6c4dc2d2415f76c4a271058d2513
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-26 19:32:34 +0200

    creation of drirectory whith form file to add new vehicle

---
+++ integrationVehicle/newVehicle.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'database.php';
+$pdo = getPdo();
+
+try{
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $brandVehicle = $_POST['brandVehicle'] ;
+    $modelsVehicle = $_POST['modelsVehicle'];
+    $energyVehicle = $_POST['energyVehicle'];
+    $yearVehicle = $_POST['yearVehicle'];
+    $nbseatsVehicle = $_POST['nbseatsVehicle'];
+    $gearboxVehicle = $_POST['gearboxVehicle'];
+    $imgVehicle = $_POST['imgVehicle'];
+    $prixLocVehicle = $_POST['prixLocVehicle'];
+    $type_idtype = $_POST['type_idtype'];
+
+    //$sth appartient à la classe PDOStatement
+    $sth = $pdo->prepare("
+        INSERT INTO Vehicle(brandVehicle,modelsVehicle,energyVehicle,yearVehicle,nbseatsVehicle,gearboxVehicle,imgVehicle,prixLocVehicle,type_idtype)
+        VALUES (:brandVehicle, :modelsVehicle, :energyVehicle, :yearVehicle, :nbseatsVehicle, :gearboxVehicle, :imgVehicle, :prixLocVehicle, :type_idtype)
+    ");
+    $sth->execute(array(
+        ':brandVehicle' => $brandVehicle,
+        ':modelsVehicle' => $modelsVehicle,
+        ':energyVehicle' => $energyVehicle,
+        ':yearVehicle' => $yearVehicle,
+        ':nbseatsVehicle' => $nbseatsVehicle,
+        ':gearboxVehicle' => $gearboxVehicle,
+        ':imgVehicle' => $imgVehicle,
+        ':prixLocVehicle' => $prixLocVehicle,
+        ':type_idtype' => $type_idtype));
+
+    echo "Entrée ajoutée",'<br>', 'Merci';
+}
+
+catch(PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+}
+?>
+<html>
+<div  class="button">
+    <button  type="submit" name="submit"><a href="form_vehicle.php">retour à la liste</a></button>
+</div>
+</html>
